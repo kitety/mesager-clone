577f91e0afbd0d8beb985d7a02acb8dd2701fcc5
kitety <aizaizuori@foxmail.com>
2023-08-27 12:53:06 +0800

    feat: 04 NextAuth Setup

---
+++ app/libs/prismadb.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prismaClient = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prismaClient;
+}
+
+export default prismaClient;
